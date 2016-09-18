a858581599cdc841b3149572490b670a332e58d9
Allen <askrr3@gmail.com>
2016-09-18 11:48:22 -0400

    odds and even

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+count = 1
+while count <= 2000:
+	if count % 2 == 1:
+		print ('number is' +' ' + str(count) +' '+ 'this is an odd number')
+	elif count % 2 == 0:
+		print ('number is' +' '+ str(count) + ' ' + 'this is an even number')
+	print count
+	count += 1 
